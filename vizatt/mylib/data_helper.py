59a3d96ed045246017e25c67313f19cd992ce412
xiaolei <xiaoleihuangccnu@gmail.com>
2018-01-21 23:20:07 -0700

    update with the model and its parameters

--- vizatt/mylib/data_helper.py
+++ vizatt/mylib/data_helper.py
@@ -8,51 +8,51 @@ from keras.preprocessing.sequence import pad_sequences
 import keras
 
 import nltk
+
 nltk.data.path.append('./resources/nltk_data/')
 from nltk.stem import SnowballStemmer
+
 stemmer = SnowballStemmer('english')
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
 np.random.seed(1337)  # for reproducibility
 
-# for word2vec model
-import gensim
-
-# save storage
-import json
 try:
-	import cPickle as pickle
+    import cPickle as pickle
 except:
-	import pickle
+    import pickle
 
 if sys.version_info[0] == 2:
-	pass
+    pass
+
 
 def load_init_properties(fpath='./resources/settings.ini'):
-	"""Load properties of initializations
-	"""
-	import configparser
-	if os.path.isfile(fpath):
-		config = configparser.ConfigParser()
-		config.read_file(open(fpath))
-		return config
-	else:
-		return None
+    """Load properties of initializations
+    """
+    import configparser
+    if os.path.isfile(fpath):
+        config = configparser.ConfigParser()
+        config.read_file(open(fpath))
+        return config
+    else:
+        return None
+
 
 def padding2sequences(X, MAX_NB_WORDS=10000, MAX_SEQUENCE_LENGTH=40, tokenizer=None):
-	"""Padding sentences."""
-	if not tokenizer:
-		if keras.__version__.startswith('2'):
-			tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
-		else:
-			tokenizer = Tokenizer(nb_words=MAX_NB_WORDS)
-		tokenizer.fit_on_texts(X)
-	sequences = tokenizer.texts_to_sequences(X)
+    """Padding sentences."""
+    if not tokenizer:
+        if keras.__version__.startswith('2'):
+            tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
+        else:
+            tokenizer = Tokenizer(nb_words=MAX_NB_WORDS)
+        tokenizer.fit_on_texts(X)
+    sequences = tokenizer.texts_to_sequences(X)
 
-	print('Found %s unique tokens.' % len(tokenizer.word_index))
-	data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
-	return data, tokenizer
+    print('Found %s unique tokens.' % len(tokenizer.word_index))
+    data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+    return data, tokenizer
+
 
 def chars_dict_builder(X):
     """
@@ -65,12 +65,13 @@ def chars_dict_builder(X):
     char_indices = dict((c, i) for i, c in enumerate(chars_set))
     return char_indices
 
+
 def encode2char(X, maxlen, char_indices):
     """
     One hot encoding for running character-level neural networks;
     However, this method always produce too large sparse matrix
     """
-    #indices_char = dict((i, c) for i, c in enumerate(chars_dict))
+    # indices_char = dict((i, c) for i, c in enumerate(chars_dict))
     char_size = len(char_indices)
 
     # convert X to indices
@@ -96,28 +97,29 @@ def encode2char(X, maxlen, char_indices):
         data[index, :, :] = sent_array
     return data
 
+
 def encode2char_bme(X, maxlen, char_indices):
     """
     One hot encoding for the sentence, but another strategy:
         each word will be modeled by begin, middle and end
     """
     char_size = len(char_indices)
-    data = np.zeros((len(X), maxlen*3, char_size),dtype=np.int)
+    data = np.zeros((len(X), maxlen * 3, char_size), dtype=np.int)
 
     for index, sent in enumerate(X):
         counter = 0
-        sent_array = np.zeros((maxlen*3, char_size))
+        sent_array = np.zeros((maxlen * 3, char_size))
         words = list(sent.lower().split())
 
         for w in words:
-            if counter >= maxlen*3:
+            if counter >= maxlen * 3:
                 break
             else:
                 begin_array = np.zeros(char_size, dtype=np.int)
                 if w[0] in char_indices:
                     ix = char_indices[w[0]]
                     begin_array[ix] = 1
-                sent_array[counter,:] = begin_array
+                sent_array[counter, :] = begin_array
                 counter += 1
 
                 mid_array = np.zeros(char_size, dtype=np.int)
@@ -126,7 +128,7 @@ def encode2char_bme(X, maxlen, char_indices):
                         if c in char_indices:
                             ix = char_indices[c]
                             mid_array[ix] = 1
-                sent_array[counter,:] = mid_array
+                sent_array[counter, :] = mid_array
                 counter += 1
 
                 end_array = np.zeros(char_size, dtype=np.int)
@@ -134,73 +136,77 @@ def encode2char_bme(X, maxlen, char_indices):
                     if w[-1] in char_indices:
                         ix = char_indices[w[-1]]
                         end_array[ix] = 1
-                sent_array[counter,:] = end_array
+                sent_array[counter, :] = end_array
                 counter += 1
         data[index, :, :] = sent_array
     return data
+
 
 def mini_batch_generator(X, y, batch_size=128):
     """
     Mini batch data generator
     """
     for i in range(0, len(X), batch_size):
-        x_sample = X[i : i+batch_size]
-        y_sample = y[i : i+batch_size]
-        yield(x_sample, y_sample)
+        x_sample = X[i: i + batch_size]
+        y_sample = y[i: i + batch_size]
+        yield (x_sample, y_sample)
+
 
 def convert_code(code):
-	if '+' in code or '-' in code:
-		if int(code[-2:]) > 0:
-			return 1
-		else:
-			return -1
-	else:
-		return 0
+    if '+' in code or '-' in code:
+        if int(code[-2:]) > 0:
+            return 1
+        else:
+            return -1
+    else:
+        return 0
+
 
 def encode_codes(codes):
-	"""Encode CODES to +1, 0, -1, such as 'O+2' and 'O+3' will be +1;
-	'O-3' and 'O-4' will be -1.
+    """Encode CODES to +1, 0, -1, such as 'O+2' and 'O+3' will be +1;
+    'O-3' and 'O-4' will be -1.
 
-	Examples:
+    Examples:
 
-	Args:
-		codes (list): list of codes
+    Args:
+        codes (list): list of codes
 
-	Return:
-		list of encoded codes
-	"""
-	return [convert_code(item) for item in codes]
+    Return:
+        list of encoded codes
+    """
+    return [convert_code(item) for item in codes]
 
 
 def preproc_data(data, use_lower=True, use_stem=False, use_stopwords=False, split_sent=False):
-	"""Functions to preprocess the dataset"""
-	dataset = []
+    """Functions to preprocess the dataset"""
+    dataset = []
 
-	for doc in data:
-		doc = doc.strip()
-		if use_lower:
-			doc = doc.lower()
+    for doc in data:
+        doc = doc.strip()
+        if use_lower:
+            doc = doc.lower()
 
-		tmp_doc = word_tokenize(doc)
-		tmp_doc = [word.strip() for word in tmp_doc if len(word.strip()) > 0]
-		if use_stem:
-			tmp_doc = [stemmer.stem(word) for word in tmp_doc]
-		if use_stopwords:
-			stopwords_set = set(stopwords.words('english'))
-			tmp_doc = [word for word in tmp_doc if word not in stopwords_set]
-		if not split_sent:
-			tmp_doc = " ".join(tmp_doc)
-		dataset.append(tmp_doc)
-	return dataset
+        tmp_doc = word_tokenize(doc)
+        tmp_doc = [word.strip() for word in tmp_doc if len(word.strip()) > 0]
+        if use_stem:
+            tmp_doc = [stemmer.stem(word) for word in tmp_doc]
+        if use_stopwords:
+            stopwords_set = set(stopwords.words('english'))
+            tmp_doc = [word for word in tmp_doc if word not in stopwords_set]
+        if not split_sent:
+            tmp_doc = " ".join(tmp_doc)
+        dataset.append(tmp_doc)
+    return dataset
+
 
 def proc_pipeline(input_data, keras_tokenizer, max_len):
-	# due to preprocessing APIs are handle a list of data
-	if type(input_data) == str:
-		input_data = unicode(input_data)
-		input_data = [input_data]
+    # due to preprocessing APIs are handle a list of data
+    if type(input_data) == str:
+        input_data = unicode(input_data)
+        input_data = [input_data]
 
-	# convert the dataset to index
-	dataset_idx = keras_tokenizer.texts_to_sequences(input_data)
-	length_data = len(dataset_idx)
-	dataset_idx = pad_sequences(dataset_idx, maxlen=max_len)
-	return dataset_idx
+    # convert the dataset to index
+    dataset_idx = keras_tokenizer.texts_to_sequences(input_data)
+    length_data = len(dataset_idx)
+    dataset_idx = pad_sequences(dataset_idx, maxlen=max_len)
+    return dataset_idx
--- vizatt/mylib/model_helper.py
+++ vizatt/mylib/model_helper.py
@@ -4,7 +4,7 @@ import os
 import json
 
 # keras
-os.environ['KERAS_BACKEND']='theano'
+os.environ['KERAS_BACKEND'] = 'theano'
 from keras.models import load_model
 from keras import backend as K
 import keras
@@ -13,17 +13,18 @@ from keras.utils import CustomObjectScope
 from keras.models import model_from_json
 
 import sys
+
 sys.setrecursionlimit(1000000)
 
 try:
-	import cPickle as pickle
+    import cPickle as pickle
 except:
-	import pickle
+    import pickle
 
 if keras.__version__.startswith('2'):
-	import keras.initializers as initializations
+    import keras.initializers as initializations
 else:
-	import keras.initializations as initializations
+    import keras.initializations as initializations
 
 
 class Attention(Layer):
@@ -101,175 +102,185 @@ class Attention(Layer):
         # and this results in NaN's. A workaround is to add a very small positive number epsilon to the sum.
         # a /= K.cast(K.sum(a, axis=1, keepdims=True), K.floatx())
         a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
-        
+
         a = K.expand_dims(a)
         weighted_input = x * a
         return K.sum(weighted_input, axis=1)
 
     def get_output_shape_for(self, input_shape):
         return input_shape[0], input_shape[-1]
-    
+
     def compute_output_shape(self, input_shape):
         return (input_shape[0], input_shape[-1])
 
+
 def validate_inputs(input_mode, content, context, kmodel, kmodel_ctxt):
-	"""This function is to validate the inputs from the Django input form
-	"""
-	if kmodel is None and kmodel_ctxt is None:
-		return 'The models do not exist in Server! Plz contact Administrators!'
+    """This function is to validate the inputs from the Django input form
+    """
+    if kmodel is None and kmodel_ctxt is None:
+        return 'The models do not exist in Server! Plz contact Administrators!'
 
-	if input_mode == None or input_mode not in ['1','2']:
-		return 'The input mode is unknown!'
-	input_mode = int(input_mode)
-	if content == None or len(content.strip())<2:
-		return 'The input content can not be empty!'
-	
-	if input_mode == 2:
-		if kmodel_ctxt is None:
-			return 'The contexutal mode can not find the contextual model! Plz contact Administrators!'
+    if input_mode == None or input_mode not in ['1', '2']:
+        return 'The input mode is unknown!'
+    input_mode = int(input_mode)
+    if content == None or len(content.strip()) < 2:
+        return 'The input content can not be empty!'
 
-		if context == None or len(context.strip())<2:
-			return 'The contextual mode requires non-empty contextual input!'
-	else:
-		if kmodel is None:
-			return 'The current mode can not find the content-only model! Plz contact Administrators!'
-	return None
-		
+    if input_mode == 2:
+        if kmodel_ctxt is None:
+            return 'The contexutal mode can not find the contextual model! Plz contact Administrators!'
+
+        if context == None or len(context.strip()) < 2:
+            return 'The contextual mode requires non-empty contextual input!'
+    else:
+        if kmodel is None:
+            return 'The current mode can not find the content-only model! Plz contact Administrators!'
+    return None
+
+
 def load_model_config(config_path='./resources/settings.ini'):
-	"""Load configuration of the model, includes input, output, attention layers
+    """Load configuration of the model, includes input, output, attention layers
 
-		Args:
-			config_path (str): the path of configuration file
-		Return:
-			model_config (dict): configurations of key-value pairs.
-	"""
-	model_config = None
-	if os.path.isfile(config_path):
-		model_config = dict()
-		try:
-			with open(config_path) as config_file:
-				for line in config_file:
-					line = line.strip()
-					if line.startswith('#'):
-						continue
-					if len(line) > 2:
-						infos = line.split('\t')
-						model_config[infos[0].strip()] = infos[1].strip()
-		except:
-			model_config = None
-	if model_config is None:
-		config_default = dict()
-		config_default['keras_model'] = './resources/keras_model/rnn_attention_pure.model'
-		config_default['keras_model_context'] = './resources/keras_model/rnn_attention_context.model'
-		config_default['keras_tokenizer'] = './resources/keras_tokenizer.pkl'
-		
-		# for layer configurations		
-		config_default['content_input_layer'] = 'content_input'
-		config_default['content_output_layer'] = 'content_bilstm'
-		config_default['context_input_layer'] = 'context_input'
-		config_default['context_output_layer'] = 'context_bilstm'
-		
-		return config_default
-	return model_config
+        Args:
+            config_path (str): the path of configuration file
+        Return:
+            model_config (dict): configurations of key-value pairs.
+    """
+    model_config = None
+    if os.path.isfile(config_path):
+        model_config = dict()
+        try:
+            with open(config_path) as config_file:
+                for line in config_file:
+                    line = line.strip()
+                    if line.startswith('#'):
+                        continue
+                    if len(line) > 2:
+                        infos = line.split('\t')
+                        model_config[infos[0].strip()] = infos[1].strip()
+        except:
+            model_config = None
+    if model_config is None:
+        config_default = dict()
+        config_default['keras_model'] = './resources/keras_model/rnn_attention_pure.model'
+        config_default['keras_model_context'] = './resources/keras_model/rnn_attention_context.model'
+        config_default['keras_tokenizer'] = './resources/keras_tokenizer.pkl'
+
+        # for layer configurations
+        config_default['content_input_layer'] = 'content_input'
+        config_default['content_output_layer'] = 'content_bilstm'
+        config_default['context_input_layer'] = 'context_input'
+        config_default['context_output_layer'] = 'context_bilstm'
+
+        return config_default
+    return model_config
+
 
 def load_keras_resources(model_path='./resources/rnn_attention_pure.model',
-	contextual_model_path='./resources/rnn_attention_context.json',
-	contextual_model_path_w='./resources/rnn_attention_context.h5',
-	tk_path='./resources/keras_tokenizer.pkl'):
-	"""It will loads resources of keras (contextual) model, keras tokenizer
-	
-	Args:
-		model_path (str): keras model path without contxts
-		contextual_model_path (str): contxtual keras model path
-		contextual_model_path_w (str): weights of contxtual keras model path
-		tk_path (str): path of tokenizer
-	"""
-	kmodel = None
-	kmodel_contxt = None
-	ktk = None
-	if os.path.isfile(model_path):
-		with CustomObjectScope({'Attention': Attention}):		
-			kmodel = load_model(model_path)
-	if os.path.isfile(contextual_model_path) and os.path.isfile(contextual_model_path_w):
-		with CustomObjectScope({'Attention': Attention}):
-			# load json of network architecture
-			kmodel_contxt = model_from_json(
-				json.dumps(json.load(open(contextual_model_path))),
-				custom_objects={'Attention': Attention})
-			# load its weights by using the same name
-			kmodel_contxt.load_weights(contextual_model_path_w)
-	if os.path.isfile(tk_path):
-		ktk = pickle.load(open(tk_path, 'rb'))
+                         contextual_model_path='./resources/rnn_attention_context.json',
+                         contextual_model_path_w='./resources/rnn_attention_context.h5',
+                         tk_path='./resources/keras_tokenizer.pkl'):
+    """It will loads resources of keras (contextual) model, keras tokenizer
 
-	return kmodel, kmodel_contxt, ktk
+    Args:
+        model_path (str): keras model path without contxts
+        contextual_model_path (str): contxtual keras model path
+        contextual_model_path_w (str): weights of contxtual keras model path
+        tk_path (str): path of tokenizer
+    """
+    kmodel = None
+    kmodel_contxt = None
+    ktk = None
+    if os.path.isfile(model_path):
+        with CustomObjectScope({'Attention': Attention}):
+            kmodel = load_model(model_path)
+    if os.path.isfile(contextual_model_path) and os.path.isfile(contextual_model_path_w):
+        with CustomObjectScope({'Attention': Attention}):
+            # load json of network architecture
+            kmodel_contxt = model_from_json(
+                json.dumps(json.load(open(contextual_model_path))),
+                custom_objects={'Attention': Attention})
+            # load its weights by using the same name
+            kmodel_contxt.load_weights(contextual_model_path_w)
+    if os.path.isfile(tk_path):
+        ktk = pickle.load(open(tk_path, 'rb'))
+
+    return kmodel, kmodel_contxt, ktk
+
 
 def get_rnn_func(keras_model, config_att=None, input_mode=1):
-	# if config is None, we will initialize with default; else it will use user-defined configuration.
-	if config_att is None:
-		config_att = dict()
-		config_att['content_input_layer'] = 'content_input'
-		#output from RNN,the layer is RNN
-		config_att['content_output_layer'] = 'content_bilstm'
-		# context configuration
-		config_att['context_input_layer'] = 'context_input'
-		config_att['context_output_layer'] = 'context_bilstm'
+    # if config is None, we will initialize with default; else it will use user-defined configuration.
+    if config_att is None:
+        config_att = dict()
+        config_att['content_input_layer'] = 'content_input'
+        # output from RNN,the layer is RNN
+        config_att['content_output_layer'] = 'content_bilstm'
+        # context configuration
+        config_att['context_input_layer'] = 'context_input'
+        config_att['context_output_layer'] = 'context_bilstm'
 
-	rnn_layer_output_list = [K.function([keras_model.get_layer(config_att['content_input_layer']).input, K.learning_phase()], [keras_model.get_layer(config_att['content_output_layer']).output])]
-	
-	if input_mode == 2:
-		# for context
-		rnn_layer_output_list.append(
-			K.function([keras_model.get_layer(config_att['context_input_layer']).input,K.learning_phase()], [keras_model.get_layer(config_att['context_output_layer']).output]))
-	return rnn_layer_output_list
+    rnn_layer_output_list = [
+        K.function([keras_model.get_layer(config_att['content_input_layer']).input, K.learning_phase()],
+                   [keras_model.get_layer(config_att['content_output_layer']).output])]
+
+    if input_mode == 2:
+        # for context
+        rnn_layer_output_list.append(
+            K.function([keras_model.get_layer(config_att['context_input_layer']).input, K.learning_phase()],
+                       [keras_model.get_layer(config_att['context_output_layer']).output]))
+    return rnn_layer_output_list
+
 
 def get_att_output(rnn_layer_func, keras_model, single_input, layer_name='content_att'):
-	if type(single_input) == list:
-		single_input = np.asarray(single_input)
-	sent_len = len([item for item in single_input[0] if item != 0])
-	output = rnn_layer_func([single_input, 0])[0] # test mode
-	eij = np.dot(output, keras_model.get_layer(layer_name).get_weights()[0])
-	eij += keras_model.get_layer(layer_name).get_weights()[1] # bias
-	att_weights = np.exp(np.tanh(eij))
+    if type(single_input) == list:
+        single_input = np.asarray(single_input)
+    sent_len = len([item for item in single_input[0] if item != 0])
+    output = rnn_layer_func([single_input, 0])[0]  # test mode
+    eij = np.dot(output, keras_model.get_layer(layer_name).get_weights()[0])
+    eij += keras_model.get_layer(layer_name).get_weights()[1]  # bias
+    att_weights = np.exp(np.tanh(eij))
 
-	# slice and exclude paddings
-	att_weights = att_weights[0][-1*sent_len:]
-	# normalization by z-score
-	att_weights = stats.zscore(att_weights)
-	att_weights = [abs(att_score) for att_score in att_weights]	
-	
-	return dict(zip([item for item in single_input[0] if item!=0], att_weights))
+    # slice and exclude paddings
+    att_weights = att_weights[0][-1 * sent_len:]
+    # normalization by z-score
+    att_weights = stats.zscore(att_weights)
+    att_weights = [abs(att_score) for att_score in att_weights]
 
-def cal_pipeline(input_content, keras_model, input_context=None, input_mode=1, rnn_layer_func_list=None, rnn_layer_func_config=None):
-	"""To calculate the attention weights
+    return dict(zip([item for item in single_input[0] if item != 0], att_weights))
 
-	Args:
-		input_content (str): the preprocessed sentence indices
-		keras_model ()
-		input_context (str): the preprocessed context sentence indices
-		input_mode (int): 1 is normal mode, 2 is contextual mode
-	"""
-	if rnn_layer_func_list == None:
-		rnn_layer_func_list = get_rnn_func(keras_model, input_mode=input_mode)
-	# get attention weights for content
-	att_weights = get_att_output(rnn_layer_func_list[0], keras_model, input_content)
 
-	if input_mode == 1:
-		# get probabilities of prediction, idx0: 0 (neutral); idx1: 1 (positive); idx2: -1 (negative)
-		pred_probs = keras_model.predict_proba(input_content).tolist()[0]
-		pred_cls = np.argmax(pred_probs) # index
-		
-		return pred_probs, pred_cls, att_weights, None
-	elif input_mode == 2:
-		pred_probs = keras_model.predict(
-			[input_context, input_content]).tolist()[0]
-		sum_probs = sum(pred_probs)
-		pred_probs = [itm/sum_probs for itm in pred_probs]
-		
-		pred_cls = np.argmax(pred_probs) # index
-		att_weights_context = get_att_output(
-			rnn_layer_func_list[1], keras_model,
-			input_context, layer_name='context_att'
-		)
-		return pred_probs, pred_cls, att_weights, att_weights_context
-	else:
-		return {'error': 'Input Mode should be either content-only(1) or with contxt(2) mode'}
+def cal_pipeline(input_content, keras_model, input_context=None, input_mode=1, rnn_layer_func_list=None,
+                 rnn_layer_func_config=None):
+    """To calculate the attention weights
+
+    Args:
+        input_content (str): the preprocessed sentence indices
+        keras_model ()
+        input_context (str): the preprocessed context sentence indices
+        input_mode (int): 1 is normal mode, 2 is contextual mode
+    """
+    if rnn_layer_func_list == None:
+        rnn_layer_func_list = get_rnn_func(keras_model, input_mode=input_mode)
+    # get attention weights for content
+    att_weights = get_att_output(rnn_layer_func_list[0], keras_model, input_content)
+
+    if input_mode == 1:
+        # get probabilities of prediction, idx0: 0 (neutral); idx1: 1 (positive); idx2: -1 (negative)
+        pred_probs = keras_model.predict_proba(input_content).tolist()[0]
+        pred_cls = np.argmax(pred_probs)  # index
+
+        return pred_probs, pred_cls, att_weights, None
+    elif input_mode == 2:
+        pred_probs = keras_model.predict(
+            [input_context, input_content]).tolist()[0]
+        sum_probs = sum(pred_probs)
+        pred_probs = [itm / sum_probs for itm in pred_probs]
+
+        pred_cls = np.argmax(pred_probs)  # index
+        att_weights_context = get_att_output(
+            rnn_layer_func_list[1], keras_model,
+            input_context, layer_name='context_att'
+        )
+        return pred_probs, pred_cls, att_weights, att_weights_context
+    else:
+        return {'error': 'Input Mode should be either content-only(1) or with contxt(2) mode'}
